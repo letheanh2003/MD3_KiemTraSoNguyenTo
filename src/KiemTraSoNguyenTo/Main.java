bbfa0129372a8639335e8f94e9229151b6696bf2
unknown <letheanhbn2003@gmail.com>
2023-03-08 08:04:35 +0700

    KiemTraSoNguyenTo

---
+++ src/KiemTraSoNguyenTo/Main.java
@@ -0,0 +1,29 @@
+package KiemTraSoNguyenTo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int number = scanner.nextInt();
+        if (number < 2){
+            System.out.println(number + " is not a prime");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number + " is a prime");
+            else
+                System.out.println(number + " is not a prime");
+        }
+    }
+}
